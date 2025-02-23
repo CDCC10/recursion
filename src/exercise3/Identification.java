abbc017b3d09503b46c98b8ab413335c122a8836
CDCC10 <canizares.cdcc@gmail.com>
2025-02-23 15:24:01 -0400

    Added javadoc to exercise 3

--- src/exercise3/Identification.java
+++ src/exercise3/Identification.java
@@ -1,5 +1,17 @@
 package exercise3;
 
+/**
+ * A person identification system in the crime fighting office wants to have
+ * faster ways to build a drawing that shows the features of a suspect it's
+ * known that the drawings of the faces are divided into three regions:
+ * the upper part, the middle part and the lower part.
+ * In the middle part, there are elements such as the nose.
+ * In the upper part, there are elements such as the hair, forehead, eyes,
+ * eyebrows.
+ * In the lower part, there are mouth and chin.
+ * From each element, in turn, certain established models can be had such as
+ * a large and fleshy mouth or small and fleshy mouth.
+ */
 public class Identification {
     private UpperFace upperFace;
     private MiddleFace middleFace;
